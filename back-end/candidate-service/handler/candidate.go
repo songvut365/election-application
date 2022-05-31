09566dbefd6ba66f087cbcc449ce9abb286168bd
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-05-31 12:57:22 +0700

    Update function CRUD candidate

--- back-end/candidate-service/handler/candidate.go
+++ back-end/candidate-service/handler/candidate.go
@@ -1,23 +1,71 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"candidate-service/config"
+	"candidate-service/model"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func GetAllCandidate(c *fiber.Ctx) error {
-	return c.SendString("Get all candidate")
+	db := config.DB
+
+	var candidates = []model.Candidate{}
+	db.Model(&model.Candidate{}).Find(&candidates)
+
+	return c.Status(fiber.StatusOK).JSON(candidates)
 }
 
 func CreateCandidate(c *fiber.Ctx) error {
-	return c.SendString("Create Candidate")
+	db := config.DB
+
+	var candidate model.Candidate
+	c.BodyParser(&candidate)
+
+	db.Create(&candidate)
+
+	return c.Status(fiber.StatusOK).JSON(candidate)
 }
 
 func GetCandidate(c *fiber.Ctx) error {
-	return c.SendString("Get Candidate")
+	db := config.DB
+
+	id := c.Params("id")
+	var candidate model.Candidate
+
+	err := db.Model(&model.Candidate{}).Where("id = ?", id).First(&candidate).Error
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "candidate not found",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(candidate)
 }
 
 func UpdateCandidate(c *fiber.Ctx) error {
-	return c.SendString("Update Candidate")
+	db := config.DB
+
+	var updateCandidate model.Candidate
+	c.BodyParser(&updateCandidate)
+
+	id := c.Params("id")
+
+	db.Model(&model.Candidate{}).Where("id = ?", id).Updates(&updateCandidate)
+
+	var candidate model.Candidate
+	db.Model(&model.Candidate{}).Where("id = ?", id).First(&candidate)
+
+	return c.Status(fiber.StatusOK).JSON(candidate)
 }
 
 func DeleteCandidate(c *fiber.Ctx) error {
-	return c.SendString("Delete Candidate")
+	db := config.DB
+
+	id := c.Params("id")
+	db.Delete(&model.Candidate{}, id)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "candidate deleted",
+	})
 }
