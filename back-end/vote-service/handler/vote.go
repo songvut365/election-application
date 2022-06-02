873ccebdb6b31c9532fd1692bad2d5f68d75263a
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-02 23:45:37 +0700

    Update check election status from electio service

--- back-end/vote-service/handler/vote.go
+++ back-end/vote-service/handler/vote.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"net/http"
 	"os"
 	"time"
 	"vote-service/config"
@@ -9,6 +11,18 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 )
+
+func httpRequest(url string, receiver interface{}) error {
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	request, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer request.Body.Close()
+
+	return json.NewDecoder(request.Body).Decode(receiver)
+}
 
 func CheckVoteStatus(c *fiber.Ctx) error {
 	db := config.MI.DB.Collection(os.Getenv("MONGODB_COLLECTION"))
@@ -42,6 +56,15 @@ func Vote(c *fiber.Ctx) error {
 	c.BodyParser(&input)
 
 	// Check election is closed (API)
+	var status model.ElectionStatus
+	httpRequest("http://localhost:5003/api/election/status", &status)
+
+	if !status.Enable {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status":  "error",
+			"message": "Election is closed",
+		})
+	}
 
 	// Find by national id
 	vote := &model.Vote{}
