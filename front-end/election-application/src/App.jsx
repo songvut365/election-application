92258abddee9e208c016acf0136b305661b0c3f0
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-09 17:39:38 +0700

    Add candidate mockup to candidate list

--- front-end/election-application/src/App.jsx
+++ front-end/election-application/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Candidate from './components/Candidate';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Election Application</h1>
+      <div className='candidateList'>
+        <Candidate />
+        <Candidate />
+        <Candidate />
+        <Candidate />
+        <Candidate />
+        <Candidate />
+        <Candidate />
+        <Candidate />
+      </div>
     </div>
   )
 }
