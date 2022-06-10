09079c0fb75c294afa3282907e327ce205b2754f
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-10 20:23:07 +0700

    Update mockup data and style

--- front-end/election-application/src/components/Candidate.jsx
+++ front-end/election-application/src/components/Candidate.jsx
@@ -20,28 +20,19 @@ function formatVotedCount(votedCount) {
   return votedCount
 }
 
-export default function Candidate() {
-  let candidate =   {
-    id: "1",
-    name: "Elon Musk",
-    dob: "June 28, 1971",
-    bioLink: "https://en.wikipedia.org/wiki/Elon_Musk",
-    imageLink: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg",
-    policy: "Choose me if your don't know who to choose",
-    votedCount: 1100
-  }
-
+export default function Candidate(props) {
+  let candidate = props.candidate;
   return (
-    <div className='max-w-sm rounded-lg shadow-lg p-4'>
+    <div className='max-w-sm rounded-lg shadow-lg p-4 bg-white relative'>
       <div className='relative'>
         <img src={candidate.imageLink} className="w-full h-96 object-cover object-top rounded-lg shadow-md" />
-        <h1 className="absolute top-0 right-0 px-4 py-2 bg-gray-200 text-5xl font-bold rounded-tr-lg">
+        <h1 className="absolute top-0 right-0 px-4 py-2 bg-gray-200 text-5xl font-bold rounded-tr-md">
           {candidate.id}
         </h1>
       </div>
 
       <div className='flex justify-between p-4'>
-        <div>
+        <div className='text-left'>
           <h3 className='font-bold text-lg'>{candidate.name}</h3>
           <p className='text-xs font-bold text-gray-400'>{ageFromDate(candidate.dob)} yrs</p>
         </div>
@@ -52,9 +43,9 @@ export default function Candidate() {
         </div>
       </div>
       
-      <h2 className='text-2xl p-4'>" {candidate.policy} "</h2>
+      <h2 className='text-2xl p-4 mb-4'>" {candidate.policy} "</h2>
 
-      <Button>VOTE</Button>
+      <Button className="absolute bottom-0">VOTE</Button>
     </div>
   )
 }
