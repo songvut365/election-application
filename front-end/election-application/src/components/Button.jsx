d3f0be631ecba50c4af57577b155daa2e6b14488
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-11 19:34:49 +0700

    Fix custom color by props not working

--- front-end/election-application/src/components/Button.jsx
+++ front-end/election-application/src/components/Button.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 
 export default function Button(props) {
-  var bgColor = " bg-"+props.bgColor+"-600 "
-  var bgColorHover = " hover:bg-"+props.bgColor+"-800 "
-  var bgColorActive = " active:bg-"+props.bgColor+"-700 "
-  var textColor = " text-"+props.textColor+" "
-  var width = " w-full "
+  var bgColor = ""
+  var bgColorHover = ""
+  var bgColorActive = ""
+  var textColor = ""
 
-  if (props.bgColor == "white") {
-    bgColor = " bg-white border-2 "
-    bgColorHover = " hover:bg-slate-200 "
-    bgColorActive = " active:bg-slate-400 "
-    textColor = " text-slate-500"
+  if (props.bgColor === "white") {
+    bgColor = " bg-white border-2"
+    bgColorHover = " hover:bg-slate-200"
+    bgColorActive = " active:bg-slate-400"
+    textColor = " text-slate-500 "
   }
 
-  const classString = "py-2 rounded-md font-semibold " 
-  + bgColor + bgColorHover +  bgColorActive + textColor + width
+  if (props.bgColor === "blue") {
+    bgColor = " bg-blue-600"
+    bgColorHover = " hover:bg-blue-800"
+    bgColorActive = " active:bg-blue-700"
+    textColor = " text-white"
+  }
+
+  var classString = "py-2 rounded-md font-semibold w-full " + bgColor + bgColorHover +  bgColorActive + textColor
 
   return (
     <button className={classString} onClick={props.method}>
