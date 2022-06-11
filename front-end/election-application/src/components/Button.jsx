5fb71d394bc9286904453d086433b525548d6122
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-11 19:08:46 +0700

    Add style to props of button

--- front-end/election-application/src/components/Button.jsx
+++ front-end/election-application/src/components/Button.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 export default function Button(props) {
+  var bgColor = " bg-"+props.bgColor+"-600 "
+  var bgColorHover = " hover:bg-"+props.bgColor+"-800 "
+  var bgColorActive = " active:bg-"+props.bgColor+"-700 "
+  var textColor = " text-"+props.textColor+" "
+  var width = " w-full "
+
+  if (props.bgColor == "white") {
+    bgColor = " bg-white border-2 "
+    bgColorHover = " hover:bg-slate-200 "
+    bgColorActive = " active:bg-slate-400 "
+    textColor = " text-slate-500"
+  }
+
+  const classString = "py-2 rounded-md font-semibold " 
+  + bgColor + bgColorHover +  bgColorActive + textColor + width
+
   return (
-    <button className='bg-blue-600 px-12 py-2 rounded-md text-white
-    hover:bg-blue-800 active:bg-blue-700'>
+    <button className={classString} onClick={props.method}>
       {props.children}
     </button>
   )
