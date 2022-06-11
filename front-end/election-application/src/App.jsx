004588d943e1298a76e440ba0d0b6f135b002004
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-11 19:12:42 +0700

    Add and handle vote box

--- front-end/election-application/src/App.jsx
+++ front-end/election-application/src/App.jsx
@@ -1,37 +1,58 @@
 import react, {useState, useEffect} from 'react';
 import axios from 'axios';
+
 import Candidate from './components/Candidate';
+import VoteBox from './components/VoteBox';
 
 function App() {
   const CANDIDATE_URL = import.meta.env.VITE_CANDIDATE_URL
   const ELECTION_URL = import.meta.env.VITE_ELECTION_URL
 
   const [candidates, setCandidates] = useState([])
+  const [candidate, setCandidate] = useState({})
   const [electionStatus, setElectionStatus] = useState({})
+  const [showVoteBox, setShowVoteBox] = useState(false)
+
+  const fetchElectionStatus = async () => {
+    const response = await axios(ELECTION_URL+"/status");
+    setElectionStatus(response.data);
+  };
+
+  const fetchCandidates = async () => {
+    const response = await axios(CANDIDATE_URL);
+    setCandidates(response.data);
+  };
 
   useEffect(() => {
-    const fetchCandidates = async () => {
-      const response = await axios(CANDIDATE_URL);
-      setCandidates(response.data);
-    };
     fetchCandidates();
-
-    const fetchElectionStatus = async () => {
-      const response = await axios(ELECTION_URL+"/status");
-      setElectionStatus(response.data);
-    };
     fetchElectionStatus();
   }, []);
 
-  return (
-    <div className="container mx-auto text-center py-8">
-      <h1 className='text-6xl mb-8 text-blue-700 font-bold'>Election Application</h1>
+  function voteBoxHandler(candidate) {
+    setShowVoteBox(!showVoteBox)
+    setCandidate(candidate)
+    fetchElectionStatus();
+    fetchCandidates();
+  }
 
-      <div className='grid gap-8 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4'>
-        {candidates.map((candidate) => (
-          <Candidate candidate={candidate} key={candidate.ID} electionStatus={electionStatus.enable} />
-        ))}
-      </div>
+  return (
+    <div>
+      {showVoteBox && <VoteBox candidate={candidate} closeVoteBox={() => voteBoxHandler()} />}
+
+      {!showVoteBox && <div className="container mx-auto text-center py-8">
+        <h1 className='text-6xl mb-8 text-blue-700 font-bold'>Election Application</h1>
+
+        <div className='grid gap-8 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4'>
+          {candidates.map((candidate) => (
+            <Candidate 
+              candidate={candidate} 
+              key={candidate.ID} 
+              electionStatus={electionStatus.enable}
+              openVoteBox={() => voteBoxHandler(candidate)}
+            />
+            ))}
+        </div>
+      </div>}
     </div>
   )
 }
