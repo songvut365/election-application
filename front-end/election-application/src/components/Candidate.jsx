6777e72a4440906c66349feef8bd81e6a4003e47
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-11 19:11:42 +0700

    Update pass method to button in candidate box

--- front-end/election-application/src/components/Candidate.jsx
+++ front-end/election-application/src/components/Candidate.jsx
@@ -23,31 +23,40 @@ function formatVotedCount(votedCount) {
 export default function Candidate(props) {
   let candidate = props.candidate;
   return (
-    <div className='max-w-sm rounded-lg shadow-lg p-4 bg-white relative'>
-      <div className='relative hover:scale-105'>
-        <img src={candidate.ImageLink} 
-          className="w-full h-96 object-cover object-top rounded-lg shadow-md" 
-        />
-        <h1 className="absolute top-0 right-0 px-4 py-2 bg-gray-200 text-5xl font-bold rounded-tr-md">
-          {candidate.ID}
-        </h1>
-      </div>
-
-      <div className='flex justify-between p-4'>
-        <div className='text-left'>
-          <h3 className='font-bold text-lg'>{candidate.Name}</h3>
-          <p className='text-xs font-bold text-gray-400'>{ageFromDate(candidate.DOB)} yrs</p>
+    <div className='max-w-sm rounded-lg shadow-lg p-4 bg-white relative flex flex-col justify-between'>
+      <div className='relative'>
+        <div className="hover:scale-105">
+          <img src={candidate.ImageLink} 
+            className="w-full h-96 object-cover object-top rounded-lg shadow-md" 
+            />
+          <h1 className="absolute top-0 right-0 px-4 py-2 bg-gray-200 text-5xl font-bold rounded-tr-md">
+            {candidate.ID}
+          </h1>
         </div>
 
-        <div>
-          <h2 className='font-bold text-xl'>{formatVotedCount(candidate.VotedCount)}</h2>
-          <p className='text-sm'>votes</p>
-        </div>
-      </div>
-      
-      <h2 className='text-2xl p-4 mb-4'>" {candidate.Policy} "</h2>
+        <div className='flex justify-between p-4'>
+          <div className='text-left'>
+            <h3 className='font-bold text-lg'>{candidate.Name}</h3>
+            <p className='text-xs font-bold text-gray-400'>{ageFromDate(candidate.DOB)} yrs</p>
+          </div>
 
-      {props.electionStatus && <Button className="absolute bottom-0">VOTE</Button>}
+          <div>
+            <h2 className='font-bold text-xl'>{formatVotedCount(candidate.VotedCount)}</h2>
+            <p className='text-sm'>votes</p>
+          </div>
+        </div>
+
+        <h2 className='text-2xl p-4 mb-4'>" {candidate.Policy} "</h2>
+      </div>
+
+      {props.electionStatus && 
+        <Button 
+          method={props.openVoteBox}
+          bgColor="blue" textColor="white"
+        >  
+          VOTE
+        </Button>
+      }
     </div>
   )
 }
