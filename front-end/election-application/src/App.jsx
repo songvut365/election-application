2a97c3565568823d9fb7d0b23c34aae389869f5d
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-12 17:26:13 +0700

    Add function find and show new mayor when election close

--- front-end/election-application/src/App.jsx
+++ front-end/election-application/src/App.jsx
@@ -10,30 +10,51 @@ function App() {
 
   const [candidates, setCandidates] = useState([])
   const [candidate, setCandidate] = useState({})
+  const [mayor, setMayor] = useState({})
   const [electionStatus, setElectionStatus] = useState({})
   const [showVoteBox, setShowVoteBox] = useState(false)
 
   const fetchElectionStatus = async () => {
-    const response = await axios(ELECTION_URL+"/status");
-    setElectionStatus(response.data);
+    await axios(ELECTION_URL+"/status").then(response => {
+      setElectionStatus(response.data);
+
+      if (response.data.enable === false) {
+        findNewMayor();
+      }
+    });
   };
 
   const fetchCandidates = async () => {
-    const response = await axios(CANDIDATE_URL);
-    setCandidates(response.data);
+    await axios.post(ELECTION_URL+"/result").then(response => {
+      setCandidates(response.data);
+    })
   };
+
+  function voteBoxHandler(candidate) {
+    setShowVoteBox(!showVoteBox)
+    setCandidate(candidate)
+    fetchCandidates();
+    fetchElectionStatus();
+  }
+
+  function findNewMayor() {
+    axios.post(ELECTION_URL+"/result").then(response => {
+      let allCandidates = response.data
+      let max =  0
+      for (let c of allCandidates) {
+        let percentageInt = parseInt(c.Percentage.substring(0,3))
+        if (max < percentageInt) {
+          max = percentageInt
+          setMayor(c)
+        }
+      }
+    })
+  }
 
   useEffect(() => {
     fetchCandidates();
     fetchElectionStatus();
   }, []);
-
-  function voteBoxHandler(candidate) {
-    setShowVoteBox(!showVoteBox)
-    setCandidate(candidate)
-    fetchElectionStatus();
-    fetchCandidates();
-  }
 
   return (
     <div>
@@ -42,11 +63,17 @@ function App() {
       {!showVoteBox && <div className="container mx-auto text-center py-8">
         <h1 className='text-6xl mb-8 text-blue-700 font-bold'>Election Application</h1>
 
+        {!electionStatus.enable && <p className='mb-8 text-xl'>
+          The new mayor is: <span className="text-3xl font-bold italic">
+            #{mayor.ID} {mayor.Name}
+          </span>
+        </p>}
+
         <div className='grid gap-8 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4'>
           {candidates.map((candidate) => (
             <Candidate 
               candidate={candidate} 
-              key={candidate.ID} 
+              key={candidate.Name} 
               electionStatus={electionStatus.enable}
               openVoteBox={() => voteBoxHandler(candidate)}
             />
