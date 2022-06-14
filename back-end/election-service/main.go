b802f3240f559f042b57ae830a458e2919c42487
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-14 22:18:21 +0700

    Add recover middleware

--- back-end/election-service/main.go
+++ back-end/election-service/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/websocket/v2"
 )
 
@@ -19,6 +20,7 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 	app.Use(cors.New())
+	app.Use(recover.New())
 
 	// Setup
 	config.SetupEnv()
