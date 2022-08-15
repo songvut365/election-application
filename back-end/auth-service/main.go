9fbefe21de848959c31b2d85f90ce3626f36140d
songvut365 <songvut.nakrong@gmail.com>
2022-08-15 12:48:24 +0700

    initial hello world with go echo

---
+++ back-end/auth-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	app := echo.New()
+
+	app.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	app.Logger.Fatal(app.Start(":5001"))
+}
